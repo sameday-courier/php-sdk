486deae336718cda41490cfd03f21bef6dc5bd73
Daniel Gorgan <danut007ro@gmail.com>
2019-02-26 13:46:51 +0200

    Post parcel response

--- src/Sameday/Responses/SamedayPostParcelResponse.php
+++ src/Sameday/Responses/SamedayPostParcelResponse.php
@@ -16,14 +16,29 @@ class SamedayPostParcelResponse implements SamedayResponseInterface
     use SamedayResponseTrait;
 
     /**
+     * @var string
+     */
+    protected $parcelAwbNumber;
+
+    /**
      * SamedayPostParcelResponse constructor.
      *
      * @param SamedayPostParcelRequest $request
      * @param SamedayRawResponse $rawResponse
+     * @param string $parcelAwbNumber
      */
-    public function __construct(SamedayPostParcelRequest $request, SamedayRawResponse $rawResponse)
+    public function __construct(SamedayPostParcelRequest $request, SamedayRawResponse $rawResponse, $parcelAwbNumber)
     {
         $this->request = $request;
         $this->rawResponse = $rawResponse;
+        $this->parcelAwbNumber = $parcelAwbNumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParcelAwbNumber()
+    {
+        return $this->parcelAwbNumber;
     }
 }
